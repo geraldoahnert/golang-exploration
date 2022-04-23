89f9141c738955a2acbc975b7ab0c7e85dd630e7
Geraldo Ahnert <geraldoahnert@gmail.com>
2022-04-23 19:15:19 -0300

    pointers in go

---
+++ fundamentos/ponteiro/ponteiro.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main(){
+	i := 1
+
+	var p *int = nil
+	
+	p = &i // pegando o endereço da variável e atribuindo pro ponteiro
+	
+	*p++ // desreferenciando (pegando o valr) e incrementando
+	i++
+	
+	// Go não tem aritimética de ponteiros.
+	// p++
+
+
+	fmt.Println(p, *p, i, &i) // 0xc0000ba000 3 3 0xc0000ba000
+}
